670ea62f0324ab1700fbb85c0eba403ecf43173d
gdbroman <99gustaf@gmail.com>
2024-04-27 11:41:54 +0200

    Search endpoint

--- code/web-app/src/pages/Products.tsx
+++ code/web-app/src/pages/Products.tsx
@@ -7,10 +7,6 @@ interface Product {
   name: string;
 }
 
-interface GetProductsQuery {
-  products: Product[];
-}
-
 const Products: React.FC = () => {
   const [newProductText, setNewProductText] = useState('');
   const [pushToKafka, setPushToKafka] = useState(false);
@@ -28,6 +24,7 @@ const Products: React.FC = () => {
         if (prev.products.some((product: Product) => product.id === newProduct.id)) {
           return prev;
         }
+
         return Object.assign({}, prev, {
           products: [...prev.products, newProduct]
         });
@@ -43,12 +40,56 @@ const Products: React.FC = () => {
       </button>
     </div>
   );
-
   if (error) return <p>{'Error: ' + error}</p>;
+
+  const handleAddProduct = async () => {
+    if (!newProductText.trim()) return;
+    if (pushToKafka) {
+      const response = await fetch('/input/add_product', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: newProductText }),
+      });
+      if (response.ok) {
+        setNewProductText('');
+      } else {
+        const errorText = await response.text();
+        console.error('Failed to add product:', errorText);
+      }
+    } else {
+      await addProduct({ variables: { name: newProductText } });
+      setNewProductText('');
+    }
+  };
 
   return (
     <div className="min-h-screen flex flex-col">
-      Put in your URL
+      <div className="navbar bg-base-300 text-neutral-content">
+        <div className="flex-1">
+          RAG search
+        </div>
+      </div>
+
+      <div className="flex flex-grow justify-center items-center bg-neutral">
+        <div className="card card-compact w-full max-w-lg bg-base-100 shadow-xl">
+          <div className="card-body items-stretch text-center">
+            <div className="form-control w-full">
+              <input
+                type="url"
+                placeholder="URL"
+                className="join-item flex-grow input input-bordered input-md input-primary"
+                value={newProductText}
+                onChange={(e) => setNewProductText(e.target.value)}
+              />
+              <button className="join-item btn btn-square btn-md btn-primary" onClick={handleAddProduct}>
+                Go
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
